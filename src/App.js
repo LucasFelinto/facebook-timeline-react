1dad87be0e960b5f58c1a6b3045a9a9eef159530
LucasFelinto <elucasfelinto@gmail.com>
2020-02-15 17:51:41 -0200

    :wrench: adicionado componente app na interface

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './reset.css';
+
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
