e1e35827a66146c335684bd9f730cec23f089ce1
LucasFelinto <elucasfelinto@gmail.com>
2020-02-15 20:01:57 -0200

    :ok_hand: adicionando componente TimeLine na interface

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './reset.css';
 
-import Header from './components/Header'
+import Header from './components/Header';
+import TimeLine from './components/TimeLine';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TimeLine />
     </div>
   );
 }
